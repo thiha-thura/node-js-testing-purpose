570fbba70223651353acbc6607dbb7e08345bd1f
thiha-thura <88715513+thiha-thura@users.noreply.github.com>
2023-07-14 11:45:05 +0630

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = 8080
 app.get('/', (req, res) => {
   res.redirect('http://ytechnologies.org/')
 })
+app.get('/health', (req,res) => {
+  res.send('Your Container is UP and Running')
+}
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
