a16503670482047fc3a28ef4ce93349b5c0cdb98
thiha-thura <88715513+thiha-thura@users.noreply.github.com>
2023-07-17 16:00:03 +0630

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Node is working!! ')
+  res.send('Node is working!!-(hc removed) ')
 })
 app.get('/live ', (req,res) => {
   res.send('Your Container is UP and Running')
