ac45e21cff27f36ab3cf8710eefb4dc394244c43
thiha-thura <88715513+thiha-thura@users.noreply.github.com>
2023-07-17 13:30:41 +0630

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Working as Root')
+  res.send('<html><li>usd</li><li>sg</li><li>mmk</li></html>')
 })
 app.get('/health', (req,res) => {
   res.send('Your Container is UP and Running')
