c34fee11c056c426883e27a0651055ec405d69a2
thiha-thura <88715513+thiha-thura@users.noreply.github.com>
2023-07-17 21:48:57 +0630

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ i = 0
 app.get('/', (req, res) => {
   res.send((i++ )+' Node is working!! ')
 })
-app.get('/health ', (req,res) => {
+app.get('/healthz ', (req,res) => {
   res.send('Your Container is UP and Running')
 })
 app.listen(port, () => {
