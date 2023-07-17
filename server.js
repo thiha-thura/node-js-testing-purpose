9b5119a22bf66182f2d6bc2c6982ed2cd671d085
thiha-thura <88715513+thiha-thura@users.noreply.github.com>
2023-07-17 15:59:34 +0630

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8080
 app.get('/', (req, res) => {
   res.send('Node is working!! ')
 })
-app.get('/health', (req,res) => {
+app.get('/live ', (req,res) => {
   res.send('Your Container is UP and Running')
 })
 app.listen(port, () => {
